860276e3b60e066a177478ad7a772b257bcd6a6e
aziz <zarroukmouhamedaziz904@gmail.com>
2025-11-09 23:09:15 +0100

    prosit8

--- src/tn/esprit/gestionzoo/entities/Aquatic.java
+++ src/tn/esprit/gestionzoo/entities/Aquatic.java
@@ -1,21 +1,24 @@
 package tn.esprit.gestionzoo.entities;
 
-public class Aquatic extends Animal {
+import tn.esprit.gestionzoo.enums.Food;
+import tn.esprit.gestionzoo.interfaces.Carnivore;
+
+public abstract class Aquatic extends Animal implements Carnivore<Food> {
 
     private String habitat;
 
-    public Aquatic(String name) {
-        super(name);
+    public Aquatic() {
+        super();
+        setFamily("Aquatic");
+        setName("Unknown Aquatic");
+        setAge(0);
+        setMammal(false);
+        setHabitat("unknown");
     }
 
-    public Aquatic(String name, String habitat) {
-        super(name);
-        this.habitat = habitat;
-    }
-    public Aquatic(String name, int age, String habitat) {
-        super(name);
-        this.setAge(age);
-        this.habitat = habitat;
+    public Aquatic(String name, int age, boolean mammal, String habitat) {
+        super("Aquatic", name, age, mammal);
+        setHabitat(habitat);
     }
 
     public String getHabitat() {
@@ -23,27 +26,31 @@ public class Aquatic extends Animal {
     }
 
     public void setHabitat(String habitat) {
-        this.habitat = habitat;
+        if (habitat == null || habitat.trim().isEmpty()) {
+            throw new IllegalArgumentException("L'habitat ne peut pas être vide.");
+        }
+        this.habitat = habitat.trim();
     }
+
+    // Méthode de l'interface Carnivore
+    @Override
+    public void eatMeat(Food meat) {
+        if (meat == Food.MEAT || meat == Food.BOTH) {
+            System.out.println(getName() + " is eating meat!");
+        } else {
+            System.out.println(getName() + " cannot eat this food.");
+        }
+    }
+
+    public abstract void swim();
+
     @Override
     public String toString() {
-        return super.toString() +"', habitat='" + habitat;
+        return "Aquatic{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", isMammal=" + isMammal() +
+                ", habitat='" + habitat + '\'' +
+                '}';
     }
-
-    public void swim() {
-        System.out.println("This aquatic animal is not swimming anymore.");
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Aquatic other = (Aquatic) obj;
-        boolean sameName = this.getName().equals(other.getName());
-        boolean sameAge = this.getAge() == other.getAge();
-        boolean sameHabitat = this.habitat.equals(other.habitat);
-
-        return sameName && sameAge && sameHabitat;
-    }
-
 }
--- src/tn/esprit/gestionzoo/entities/Terrestre.java
+++ src/tn/esprit/gestionzoo/entities/Terrestre.java
@@ -1,13 +1,71 @@
 package tn.esprit.gestionzoo.entities;
 
-public class Terrestre extends Animal{
+import tn.esprit.gestionzoo.enums.Food;
+import tn.esprit.gestionzoo.interfaces.Omnivore;
+
+public class Terrestrial extends Animal implements Omnivore<Food> {
+
     private int nbrLegs;
-    public Terrestre(String name, int nbrLegs){
-        super(name);
+
+    public Terrestrial() {
+        super();
+        setFamily("Terrestrial");
+        setName("Unknown Terrestrial");
+        setAge(0);
+        setMammal(true);
+        setNbrLegs(4);
+    }
+
+    public Terrestrial(String name, int age, boolean mammal, int nbrLegs) {
+        super("Terrestrial", name, age, mammal);
+        setNbrLegs(nbrLegs);
+    }
+
+    public int getNbrLegs() {
+        return nbrLegs;
+    }
+
+    public void setNbrLegs(int nbrLegs) {
+        if (nbrLegs < 0) {
+            throw new IllegalArgumentException("Le nombre de pattes ne peut pas être négatif.");
+        }
         this.nbrLegs = nbrLegs;
     }
+
+    @Override
+    public void eatMeat(Food meat) {
+        if (meat == Food.MEAT || meat == Food.BOTH) {
+            System.out.println(getName() + " is eating meat!");
+        } else {
+            System.out.println(getName() + " cannot eat meat.");
+        }
+    }
+
+    @Override
+    public void eatPlant(Food plant) {
+        if (plant == Food.PLANT || plant == Food.BOTH) {
+            System.out.println(getName() + " is eating plants!");
+        } else {
+            System.out.println(getName() + " cannot eat plants.");
+        }
+    }
+
+    @Override
+    public void eatPlantAndMeat(Food food) {
+        if (food == Food.BOTH) {
+            System.out.println(getName() + " is eating both plants and meat!");
+        } else {
+            System.out.println(getName() + " cannot eat this food.");
+        }
+    }
+
     @Override
     public String toString() {
-        return "Terrestrial{name=" + name + ", nbrLegs=" + nbrLegs + "}";
+        return "Terrestrial{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", isMammal=" + isMammal() +
+                ", nbrLegs=" + nbrLegs +
+                '}';
     }
 }
